c19d268acad96bf142d536a3c77ab36681c1e516
Sarthak <sarthak@gravitii.xyz>
2024-05-24 14:21:50 +0530

    Adiing all the files

--- 15Friend3.cpp
+++ 15Friend3.cpp
@@ -6,7 +6,7 @@ class A{
     public:
     A(){num1=20;}
     friend void show(A &,B &);           //three friend functions with objects of different classes
-    friend void getvalue(A &,B &);
+    friend void getvalue(A &,B &);  
     friend void add(A &,B &);
 };
 class B{
@@ -18,22 +18,15 @@ class B{
     friend void add(A &,B &);
 };
 void getvalue(A &obj1,B &obj2)
-{cout<<"Give value for object of A"<<endl;
-cin>>obj1.num1;
-cout<<"Give value for object of B"<<endl;
-cin>>obj2.num2;
-}
+{cout<<"Give value for object of A"<<endl; cin>>obj1.num1;
+cout<<"Give value for object of B"<<endl;  cin>>obj2.num2;}
 void add(A &obj1,B &obj2)
-{
-    cout<<"After adding we get the result as "<<obj1.num1+obj2.num2<<endl;
-}
+{cout<<"After adding we get the result as "<<obj1.num1+obj2.num2<<endl;}
 void show(A &obj1,B &obj2)
 {cout<<"Value for class A object is"<<obj1.num1<<endl;
 cout<<"Value for class B object is"<<obj2.num2<<endl;}
-
 int main()
-{
-    A obj1;
+{   A obj1;
     B obj2;
     show(obj1,obj2);
     getvalue(obj1,obj2);
